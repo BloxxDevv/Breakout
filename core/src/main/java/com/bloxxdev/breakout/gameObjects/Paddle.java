4067a8f6b4d2030ea6ce2200ce91fda868510da9
alex <bloxxydev@gmail.com>
2025-03-28 09:43:53 +0100

    Finished Paddle movement

--- core/src/main/java/com/bloxxdev/breakout/gameObjects/Paddle.java
+++ core/src/main/java/com/bloxxdev/breakout/gameObjects/Paddle.java
@@ -1,20 +1,52 @@
 package com.bloxxdev.breakout.gameObjects;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 
 public class Paddle implements GameObject{
 
+    public static final int LEFT = 0;
+    public static final int RIGHT = 1;
+
     public static int PADDLE_WIDTH = 100;
     public static final int PADDLE_HEIGHT = 10;
 
-    public static final int SPEED = 5;
-    public static final int SCALE = 5;
+    public static final int SPEED = 8;
 
     private int x;
     private int y;
 
     private ShapeRenderer renderer;
+
+    private boolean[] movement = new boolean[]
+    {
+        false,
+        false
+    };
+
+    public void setMovement(int direction, boolean value) {
+        this.movement[direction] = value;
+    }
+
+    //Move player if not at border. if movement would pass the border, set pos at border
+    private void move(){
+        if (movement[LEFT] != movement[RIGHT]) {
+            if (movement[LEFT]) {
+                if (x-SPEED >= 0) {
+                    x-=SPEED;
+                }else {
+                    x = 0;
+                }
+            }else{
+                if (x+SPEED <= Gdx.graphics.getWidth() - Paddle.PADDLE_WIDTH) {
+                    x+=SPEED;
+                }else{
+                    x = Gdx.graphics.getWidth() - Paddle.PADDLE_WIDTH;
+                }
+            }
+        }
+    }
 
     public Paddle(int x, int y){
         this.x = x;
@@ -25,7 +57,7 @@ public class Paddle implements GameObject{
 
     @Override
     public void tick() {
-        
+        move();
     }
 
     @Override
--- core/src/main/java/com/bloxxdev/breakout/screens/Breakout.java
+++ core/src/main/java/com/bloxxdev/breakout/screens/Breakout.java
@@ -1,6 +1,7 @@
 package com.bloxxdev.breakout.screens;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.ScreenAdapter;
 import com.bloxxdev.breakout.gameObjects.GameObject;
 import com.bloxxdev.breakout.gameObjects.Paddle;
@@ -9,6 +10,20 @@ public class Breakout extends ScreenAdapter{
 
     GameObject paddle;
     
+    private void checkKeys(){
+        if (Gdx.input.isKeyPressed(Input.Keys.A)) {
+            ((Paddle) paddle).setMovement(Paddle.LEFT, true);
+        }else{
+            ((Paddle) paddle).setMovement(Paddle.LEFT, false);
+        }
+
+        if (Gdx.input.isKeyPressed(Input.Keys.D)) {
+            ((Paddle) paddle).setMovement(Paddle.RIGHT, true);
+        }else{
+            ((Paddle) paddle).setMovement(Paddle.RIGHT, false);
+        }
+    }
+
     @Override
     public void show() {
         Gdx.gl.glClearColor(20/255.0F, 20/255.0F, 100/255.0F, 0);
@@ -17,6 +32,8 @@ public class Breakout extends ScreenAdapter{
     }
 
     public void tick(){
+        checkKeys();
+
         paddle.tick();
     }
 
